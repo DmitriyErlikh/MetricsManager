8adeeab3d2aefae0443c7d4257deb2c82444b1e0
dmitriyerlikh@gmail.com <dmitriyerlikh@gmail.com>
2021-06-09 00:33:44 +0400

    Добавьте файлы проекта.

---
+++ MetricsManager/Controllers/CrudController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/crud")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        private readonly ValuesHolder _holder;
+
+        public CrudController(ValuesHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] string input)
+        {
+            _holder.Value.Add(input);
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(_holder.Value);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] string stringsToUpdate, [FromQuery] string newValue)
+        {
+            for (int i = 0; i < _holder.Value.Count; i++)
+            {
+                if (_holder.Value[i] == stringsToUpdate)
+                    _holder.Value[i] = newValue;
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string stringsToDelete)
+        {
+            _holder.Value = _holder.Value.Where(w => w != stringsToDelete).ToList();
+            return Ok();
+        }
+    }
+}
